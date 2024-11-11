1c9a73f9814da263512bca265cfba9f5a74eed39
Juan Domínguez Álvarez <91067589+UO277646@users.noreply.github.com>
2024-11-11 21:58:42 +0100

    Pdf funcionando

--- frontend/src/app/services/upload.service.ts
+++ frontend/src/app/services/upload.service.ts
@@ -7,13 +7,21 @@ import { ObjetoImagenResponse } from '../file-upload/file-upload.component';
   providedIn: 'root'
 })
 export class UploadService {
+  getProcedurePDF=async(arg0: any)=> {
+    
+    const response = await firstValueFrom(
+      this.http.get(this.apiPdf + arg0, { responseType: 'arraybuffer' }) // Especifica 'arraybuffer' para manejar binarios
+    );
+    console.log(response);
+    return response;
+  }
   
 
   constructor(private http:HttpClient) { }
   
   private apiUrl = 'http://localhost:8080/detect';  // Cambia esto a la URL correcta si es diferente
 
-  
+  private apiPdf = 'http://localhost:8080/generate/'; 
 
   public getDetectionResults=async(foto:any)=> {
     const response:ObjetoImagenResponse=await firstValueFrom(this.http.post<ObjetoImagenResponse>(this.apiUrl,foto));
